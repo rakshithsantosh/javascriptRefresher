d514a89ac9ef67d186d843bf82fb90a8ca801fc7
rakshith_s <rakshithsantosh@gmail.com>
2024-05-06 06:50:05 +0530

    inheritance

--- classes.js
+++ classes.js
@@ -19,3 +19,11 @@ let person = new Person("John", 30);
 //any method named constructor will be called at the time of object creation
 
 //function placed in a class are called methods (instance methods) they have access to all properties of the object with this keyword 
+
+class Person2 extends Person {
+    describe() {
+        console.log(this.name + " is " + this.age + " years old");
+    }
+}
+
+// we can use extends to inherit methods from another class
