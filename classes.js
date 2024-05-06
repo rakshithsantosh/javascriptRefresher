c5be78387e64fed698e61fae75e70283ca8ce4ff
rakshith_s <rakshithsantosh@gmail.com>
2024-05-06 06:42:29 +0530

    two liner about methods

--- classes.js
+++ classes.js
@@ -17,3 +17,5 @@ let person = new Person("John", 30);
 //we can use insatanceof to check if an object is an instance of a class
 
 //any method named constructor will be called at the time of object creation
+
+//function placed in a class are called methods (instance methods) they have access to all properties of the object with this keyword 
